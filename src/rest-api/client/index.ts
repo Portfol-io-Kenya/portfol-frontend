295d04f2777d4acdd24f5de92977da82921b8717
Billie-K <billiemush@gmail.com>
2023-11-07 18:28:39 +0200

    Update index.ts

--- src/rest-api/client/index.ts
+++ src/rest-api/client/index.ts
@@ -1,30 +1,30 @@
-import {
-    LoginUserInput,
-    AuthResponse,
-    User,
-    OpportunityQueryOptions,
-    OpportunityPaginator,
-    Opportunity
-} from '@/types';
-import { API_ENDPOINTS } from './api-endpoints';
-import { HttpClient } from './http-client';
+// import {
+//     LoginUserInput,
+//     AuthResponse,
+//     User,
+//     OpportunityQueryOptions,
+//     OpportunityPaginator,
+//     Opportunity
+// } from '@/types';
+// import { API_ENDPOINTS } from './api-endpoints';
+// import { HttpClient } from './http-client';
 
-  class Client {
-    users = {
-      login: (input: LoginUserInput) =>
-        HttpClient.post<User>(API_ENDPOINTS.LOGIN_URL, input),
-      logout: () => HttpClient.post<boolean>(API_ENDPOINTS.LOGOUT, {}),
-    };
+//   class Client {
+//     users = {
+//       login: (input: LoginUserInput) =>
+//         HttpClient.post<User>(API_ENDPOINTS.LOGIN_URL, input),
+//       logout: () => HttpClient.post<boolean>(API_ENDPOINTS.LOGOUT, {}),
+//     };
 
-    opportunities = {
-      all: (params: Partial<OpportunityQueryOptions>) =>
-        HttpClient.get<OpportunityPaginator>(
-          API_ENDPOINTS.OPPORTUNITIES,
-          params
-        ),
-      get: (profileId: string) =>
-          HttpClient.get<Opportunity>(`${API_ENDPOINTS.OPPORTUNITIES}/myopportunities/${profileId}`),
-    };
-}
+//     opportunities = {
+//       all: (params: Partial<OpportunityQueryOptions>) =>
+//         HttpClient.get<OpportunityPaginator>(
+//           API_ENDPOINTS.OPPORTUNITIES,
+//           params
+//         ),
+//       get: (profileId: string) =>
+//           HttpClient.get<Opportunity>(`${API_ENDPOINTS.OPPORTUNITIES}/myopportunities/${profileId}`),
+//     };
+// }
 
-export default new Client();
+// export default new Client();
