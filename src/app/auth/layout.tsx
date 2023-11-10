c32e9f7329985b098cb0676bb2dac10642c65992
Billie-K <billiemush@gmail.com>
2023-11-10 18:41:45 +0200

    Update layout.tsx

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -9,18 +9,24 @@ const AuthLayout = ({
     return (
         <div className='flex h-screen  items-center justify-center'>
             <div 
-                className="basis-1/2 h-full flex items-center justify-center bg-gray-800"
+                className="basis-1/2 h-full"
+                style={{
+                    backgroundImage: "url(/media/portfol-logos/portfol-15.webp)",
+                    backgroundSize: "cover"
+                }}
             >
-                <Card 
-                    className='w-[450px] rounded-lg'
-                >
-                    {children}
-                </Card>
+                <div className="bg-gray-800/80 h-full w-full flex items-center justify-center">
+                    <Card 
+                        className='w-[450px] rounded-xl py-14 px-8'
+                    >
+                        {children}
+                    </Card>
+                </div>
             </div>
             <div 
                 className="basis-1/2 h-full w-full"
                 style={{
-                    backgroundImage: "url(/media/portfol-logos/portfol-13.webp)",
+                    backgroundImage: "url(/media/portfol-logos/portfol-42.webp)",
                     backgroundSize: "cover"
                 }}
             >
