737efbf598c3713a7275b51e321c19b082751371
Billie-K <billiemush@gmail.com>
2023-11-10 18:45:20 +0200

    Update SocialLogin.tsx

--- src/components/auth/SocialLogin.tsx
+++ src/components/auth/SocialLogin.tsx
@@ -1,44 +1,58 @@
-import Image from 'next/image';
 import React from 'react';
-import AppleLogo from '@/public/media/svg/brand-logos/apple-black.svg'
+import Image from 'next/image';
+import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import AppleLogo from '@/public/media/svg/brand-logos/facebook-4.svg'
 import GoogleLogo from '@/public/media/svg/brand-logos/google-icon.svg'
 
 const SocialLogin = () => {
-    return (
-        <div>
-            <div className='flex'>
-            <div className="my-4 mr-2 flex h-[50px] w-full items-center justify-center gap-2 rounded-xl bg-gray-100 hover:bg-gray-200 hover:cursor-pointer ">
-              <div className="rounded-full text-xl">
-                <Image
-                  alt='Google-Logo'
-                  src={GoogleLogo}
-                  className=''
-                  width={25}
-                  height={25}
-                />
-              </div>
-              <h5 className="text-sm font-medium text-navy-700 ">
-                Sign In with Google
-              </h5>
-            </div>
+  const searchParams = useSearchParams()!;
+  let callbackUrl = searchParams.get("callbackUrl")!;
 
-            <div className="my-4 flex h-[50px] w-full items-center justify-center gap-2 rounded-xl bg-gray-100 hover:bg-gray-200 hover:cursor-pointer ">
-              <div className="rounded-full text-xl">
-                <Image
-                  alt='Google-Logo'
-                  src={AppleLogo}
-                  className=''
-                  width={25}
-                  height={25}
-                />
-              </div>
-              <h5 className="text-sm font-medium text-navy-700">
-                Sign In with Apple
-              </h5>
-            </div>
+  if (callbackUrl === null) {
+    callbackUrl = "/find-work/jobs";
+  }
+  return (
+    <div>
+      <div className='flex'>
+        <div 
+          onClick={() => signIn("google", { callbackUrl })}
+          className="my-4 mr-2 flex h-[50px] w-full items-center justify-center gap-2 rounded-xl bg-gray-100 hover:bg-gray-200 hover:cursor-pointer "
+        >
+          <div className="rounded-full text-xl">
+            <Image
+              alt='Google-Logo'
+              src={GoogleLogo}
+              className=''
+              width={25}
+              height={25}
+            />
           </div>
+          <h5 className="text-sm font-medium text-navy-700 ">
+            Google Login
+          </h5>
         </div>
-    );
+
+        <div 
+          onClick={() => signIn("facebook", { callbackUrl })}
+          className="my-4 flex h-[50px] w-full items-center justify-center gap-2 rounded-xl bg-gray-100 hover:bg-gray-200 hover:cursor-pointer "
+        >
+          <div className="rounded-full text-xl">
+            <Image
+              alt='Google-Logo'
+              src={AppleLogo}
+              className=''
+              width={25}
+              height={25}
+            />
+          </div>
+          <h5 className="text-sm font-medium text-navy-700">
+            Facebook Login
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SocialLogin;
