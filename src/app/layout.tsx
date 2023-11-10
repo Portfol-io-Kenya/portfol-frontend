2cded80feaffecc82ebf3042abbb3e12bc1f7acc
Billie-K <billiemush@gmail.com>
2023-11-10 19:16:30 +0200

    query-providers, assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import clsx from 'clsx'
-import QueryProvider from '@/rest-api/query-provider'
+import { QueryProvider } from '@/rest-api/query-provider';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 // Assets
 import '@/assets/globals.css'
 import '@/assets/iconly/css/style.css'
+import { NextAuthProvider } from './Provider';
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -22,7 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={clsx('flex min-h-screen flex-col justify-between', montserrat.className)}>
-        <QueryProvider>{children}</QueryProvider>
+        <NextAuthProvider>
+          <QueryProvider>
+            {children}
+            <ToastContainer />
+          </QueryProvider>
+        </NextAuthProvider>
       </body>
     </html>
   )
